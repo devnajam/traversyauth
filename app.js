bae78cec44aa3916a2a4a21c7bd1706103fa6248
angrynajam <najamiftikhar2020@gmail.com>
2020-04-10 09:53:35 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
+//pasport config
+require('./config/passport')(passport);
+
+//DB Config
+const db = require('./config/keys').MongoUri;
+
+//connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(`Database connected`))
+  .catch((err) => console.log(`Database connection error: ${err.message}`));
+
+//ejs
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+//express session
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect flash
+app.use(flash());
+
+//global vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+//routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
